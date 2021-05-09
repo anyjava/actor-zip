933696cb1d9e0558727bb27fe0563d5283851ed8
cuddy.son <cuddy.son@kakaocorp.com>
2021-05-09 18:34:51 +0900

    MOD: fix cors

--- src/main/kotlin/dev/anyjava/actorzip/adapter/presentation/config/WebConfig.kt
+++ src/main/kotlin/dev/anyjava/actorzip/adapter/presentation/config/WebConfig.kt
@@ -1,11 +1,7 @@
 package dev.anyjava.actorzip.adapter.presentation.config
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import org.springframework.web.filter.CorsFilter
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -17,18 +13,11 @@ class WebConfig : WebMvcConfigurer {
             .addResourceLocations("classpath:/static/swagger.html")
     }
 
-    @Bean
-    fun corsFilter(): FilterRegistrationBean<*>? {
-        val source = UrlBasedCorsConfigurationSource()
-        val config = CorsConfiguration()
-        config.allowCredentials = true
-        config.addAllowedOrigin("*")
-        config.addAllowedHeader("*")
-        config.addAllowedMethod("*")
-        source.registerCorsConfiguration("/v2/api-docs", config)
-        source.registerCorsConfiguration("/**", config)
-        val bean = FilterRegistrationBean(CorsFilter(source))
-        bean.order = 0
-        return bean
+    @Override
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowedHeaders("*")
+            .allowedMethods("*")
     }
 }
